85d632197518ec2937246afd531adc37fc2d91cd
GooseKIller <72573175+GooseKIller@users.noreply.github.com>
2022-02-11 09:47:31 +0300

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -1,57 +1,62 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
-
+#include <iomanip>
 using namespace std;
 
-
-int choise(int you,int tout) {
-	srand(time(NULL));
-	int card;
-	switch (you)
-	{
-	case 0:
-		tout = tout;
-		return tout;
-		break;
-	case 1:
-		card = rand() % 10 + 6;
-		tout = tout + card;
-		return tout;
-		break;
-	default:
-		cout << "wrong command";
-		break;
-	}
-}
 int main()
-{   
-	cout << "21 GAME\n\ngame rules:\n 1-take a card\n 0-don`t take a card\n\n";
+{
     srand(time(NULL));
-    int you = 0,youint,ai=0,round=0,airand=0;
-	while (ai<=21 || you<=21)
-	{	
-		if (round == 4) {
-				break;
-		}
-		cout << "ROUND:" << round + 1 << "/4\n\n";
-		cout << "YOU:"; cin >> youint;
-		you=choise(youint, you);
-		cout <<"YOU:" << you<<"\n";
-		airand = rand() % 2;
-		cout << "COMPUTER:"<<airand<< "\n";
-		ai = choise(airand, ai);
-		cout << "COMPUTER:" << ai<<"\n";
-		round++;
-		cout << endl;
-	}
-	if (ai > you && ai<=21 || you>21 && ai<=21) {
-		cout << "COMPUTER Win!!";
-	}
-	else if (ai==you)
-	{
-		cout << "COMPUTER and YOU Win!!";
-	}
-	else if(you>ai && you<=21 || ai>21  && you<=21){
-		cout << "YOU Win!!";
-	}
+    const int si = 100;
+    int b, arr[si][si];
+    int n = 15, m = 5, c = 1, e = 1, life = 30, place = 0;
+    for (int p = 0; p < 15; p++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j == 0) {
+                    arr[i][j] = c;
+                }
+                else if (j == m - 1) {
+                    arr[i][j] = c;
+                }
+                else if (j == 2) {
+                    arr[i][j] = c;
+                }
+                else if (e % 2 == 0) {
+                    arr[i][j] = 0;
+                    e++;
+                }
+                else {
+                    arr[i][j] = 1;
+                    e++;
+                }
+            }c++;
+        }
+	        int a = rand() % 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << setw(2) << arr[i][j] << " ";
+            }cout << endl;
+        }
+        if (life >= 1 && place!=n) {
+            cin >> b;
+        }
+        else {
+            cout << a << endl;
+        }
+        if (b == a) {
+            place += 1;
+            cout << place << ":place" << " " << life << ":survivers" << endl;
+        }
+        else {
+            life -= 1;
+            cout << place << ":place" << " " << life << ":survivers" << endl;
+        }c = 1;
+    }
+    if (life >= 1 && place==n) {
+        cout << "win";
+    }
+    else  if (life == 0 && place<n){
+        cout << "lose";
+    }
+    return 0;
 }
